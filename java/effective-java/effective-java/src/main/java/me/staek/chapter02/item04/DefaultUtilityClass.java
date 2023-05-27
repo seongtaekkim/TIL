105bf3064d36f7fc4a82a7a2f2713efbe8addca3
seongtaekkim <chxortnl@gmail.com>
2023-05-27 21:39:40 +0900

    Test: ADD utility 상속클래스 인스턴스 생성

---
+++ java/effective-java/effective-java/src/main/java/me/staek/chapter02/item04/DefaultUtilityClass.java
@@ -0,0 +1,9 @@
+package me.staek.chapter02.item04;
+
+public class DefaultUtilityClass extends UtilityClass {
+
+    public static void main(String[] args) {
+        DefaultUtilityClass utilityClass = new DefaultUtilityClass();
+        utilityClass.hello();
+    }
+}
