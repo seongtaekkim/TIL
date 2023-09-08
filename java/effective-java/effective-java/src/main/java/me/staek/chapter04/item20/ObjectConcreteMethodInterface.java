3dad0654f5766ca1c2fa6d1d81adcf2e4e216117
seongtaekkim <chxortnl@gmail.com>
2023-09-08 13:42:15 +0900

    Test: ADD Object concrete method by interface

---
+++ java/effective-java/effective-java/src/main/java/me/staek/chapter04/item20/ObjectConcreteMethodInterface.java
@@ -0,0 +1,20 @@
+package me.staek.chapter04.item20;
+
+/**
+ * 인터페이스는 Object의 메서드를 구현할 수 없다. (compile error)
+ */
+public interface ObjectConcreteMethodInterface {
+
+//    default String toString() {
+//        return "myString";
+//    }
+//
+//    default int hashCode() {
+//        return 10;
+//    }
+//
+//    default boolean equals(Object o) {
+//        return true;
+//    }
+
+}
