96266914f6f55e54ec494cbc39754d0cc87ca7bf
seongtaekkim <chxortnl@gmail.com>
2023-09-20 12:15:46 +0900

    Test: ADD array to generic test #31
    - 배열에 다른 타입이 입력되면 런타임에 오류가 발생할 수 있어
    - 제네릭리스트로 변경하여 해결한다.

---
+++ java/effective-java/effective-java/src/main/java/me/staek/chapter05/item28/Chooser_generic_list.java
@@ -0,0 +1,31 @@
+package me.staek.chapter05.item28;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Chooser_generic_list<T> {
+    private final List<T> choiceList;
+
+    public Chooser_generic_list(Collection<T> choices) {
+        choiceList = new ArrayList<>(choices);
+    }
+
+    public T choose() {
+        Random rnd = ThreadLocalRandom.current();
+        return choiceList.get(rnd.nextInt(choiceList.size()));
+    }
+
+    public static void main(String[] args) {
+        List<Integer> intList = List.of(1, 2, 3, 4, 5, 6);
+
+        Chooser_generic_list<Integer> chooser = new Chooser_generic_list<>(intList);
+
+        for (int i = 0; i < 10; i++) {
+            Number choice = chooser.choose();
+            System.out.println(choice);
+        }
+    }
+}
