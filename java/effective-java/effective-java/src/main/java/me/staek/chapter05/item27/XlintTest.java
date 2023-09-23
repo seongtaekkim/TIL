82b06dcc2a01442311bb73a077fb28287d5e91ee
seongtaekkim <chxortnl@gmail.com>
2023-09-23 12:42:06 +0900

    Docs: ADD item27 note
    - item27 정리
    - XlintTest 예제 수정

--- java/effective-java/effective-java/src/main/java/me/staek/chapter05/item27/XlintTest.java
+++ java/effective-java/effective-java/src/main/java/me/staek/chapter05/item27/XlintTest.java
@@ -8,13 +8,14 @@ import java.util.Set;
  * console  ->> javac XlintTest.java -Xlint:unchecked
  *
  * 위 옵션으로 컴파일을 하면 워닝이 출려된다고 하는데 나는 안됨.. 왜지? (jvm11, 17)
+ * -> names.add("aeg"); 이렇게 제네릭을 사용하니까 경고가 잘 뜬다.
  *
  */
 public class XlintTest {
 
     public static void main(String[] args) {
         Set names = new HashSet();
-
+        names.add("aeg");
         Set<String> strings = new HashSet<>();
     }
 }
