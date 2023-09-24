6306c017468c17dee8ee43c44496ce2d6f966451
seongtaekkim <chxortnl@gmail.com>
2023-09-24 12:15:59 +0900

    Test: ADD Generic Dao test #28

---
+++ java/effective-java/effective-java/src/main/java/me/staek/chapter05/item26/genericdao/after/Account.java
@@ -0,0 +1,30 @@
+package me.staek.chapter05.item26.genericdao.after;
+
+public class Account implements Entity {
+
+    private Long id;
+
+    private String username;
+
+    public Account(Long id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    @Override
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
