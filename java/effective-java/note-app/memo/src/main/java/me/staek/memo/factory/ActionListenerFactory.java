d3bf5d2589192128c8d89b3d4a5993d88f34407a
seongtaekkim <chxortnl@gmail.com>
2023-10-07 01:00:49 +0900

    Refactor: ADD ActionListener, menu 생성해서 map에 세팅하는 factory 추가

---
+++ java/effective-java/note-app/memo/src/main/java/me/staek/memo/factory/ActionListenerFactory.java
@@ -0,0 +1,39 @@
+package me.staek.memo.factory;
+
+
+import me.staek.memo.*;
+import me.staek.memo.listener.*;
+
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ActionListenerFactory implements AutoCloseable {
+
+    private final MemoFrame frame;
+    private final Map<String, ActionListener> menuMap = new HashMap<>();
+    public ActionListener actionListener(String menu) {
+        return menuMap.get(menu);
+    }
+
+    public ActionListenerFactory(MemoFrame frame) {
+        this.frame = frame;
+
+        ActionListenerStrategy fl = new FileMenuListener();
+        menuMap.put(fl.name(), fl.createListener(new FileMenu(frame)));
+
+        ActionListenerStrategy fl2 = new FontMenuListener();
+        menuMap.put(fl2.name(), fl2.createListener(new FormatMenu(frame)));
+
+        ActionListenerStrategy fsl = new FontSizeMenuListener();
+        menuMap.put(fsl.name(), fsl.createListener(new FormatMenu(frame)));
+
+        ActionListenerStrategy el = new EditMenuListener();
+        menuMap.put(el.name(), el.createListener(new EditMenu(frame)));
+    }
+
+    @Override
+    public void close() throws Exception {
+        menuMap.clear();
+    }
+}
