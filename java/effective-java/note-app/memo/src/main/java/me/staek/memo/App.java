0b67cbcd3c53091624db4fad2145429ff32cd075
seongtaekkim <chxortnl@gmail.com>
2023-10-07 18:59:49 +0900

    Feat: ADD config저장할 폴더 생성

--- java/effective-java/note-app/memo/src/main/java/me/staek/memo/App.java
+++ java/effective-java/note-app/memo/src/main/java/me/staek/memo/App.java
@@ -2,13 +2,23 @@ package me.staek.memo;
 
 import me.staek.memo.factory.ActionListenerFactory;
 
-public class App {
-    public static void main(String[] args) throws Exception {
+import java.io.File;
 
+public class App {
+    public static void main(String[] args) {
+        // setting
+        File file = new File("cfg");
+        if (!file.exists()) {
+            file.mkdir();
+        }
+
+        // program 실행
         MemoTextArea textArea = new MemoTextArea();
         MemoFrame memo = new MemoFrame(textArea);
         try (ActionListenerFactory factory = new ActionListenerFactory(memo)) {
             memo.start(factory);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
