9984833a5cc79ce856e31327adbe78d7c996ee9f
seongtaekkim <chxortnl@gmail.com>
2023-10-07 20:07:27 +0900

    Feat: ADD 파일설정 class를 byte로 바꾸어주는 기능추가

---
+++ java/effective-java/note-app/memo/src/main/java/me/staek/memo/common/Util.java
@@ -0,0 +1,21 @@
+package me.staek.memo.common;
+
+import java.io.*;
+
+public class Util {
+    public static byte[] convertObjectToBytes(Object obj) throws IOException {
+        ByteArrayOutputStream boas = new ByteArrayOutputStream();
+        try (ObjectOutputStream ois = new ObjectOutputStream(boas)) {
+            ois.writeObject(obj);
+            return boas.toByteArray();
+        }
+    }
+
+    public static Object convertBytesToObject(byte[] bytes)
+            throws IOException, ClassNotFoundException {
+        InputStream is = new ByteArrayInputStream(bytes);
+        try (ObjectInputStream ois = new ObjectInputStream(is)) {
+            return ois.readObject();
+        }
+    }
+}
