d7c3e2510579876d45e48bee4414ce5058eb8565
seongtaekkim <chxortnl@gmail.com>
2023-10-07 13:59:16 +0900

    Refactor: EditMenu 템플릿패턴 구성
    - EditMenu 추상클래스로 변경
    - UndoItem가 EditMenu를 상속받게 만들고, UndoItem은 Undo관련 업무만 하도록 개발

--- java/effective-java/note-app/memo/src/main/java/me/staek/memo/EditMenu.java
+++ java/effective-java/note-app/memo/src/main/java/me/staek/memo/EditMenu.java
@@ -2,25 +2,12 @@ package me.staek.memo;
 
 import me.staek.memo.code.Menu;
 
-public class EditMenu implements MemoMenu {
+public abstract class EditMenu implements MemoMenu {
     MemoFrame memoFrame;
     public EditMenu(MemoFrame memoFrame) {
         this.memoFrame = memoFrame;
     }
 
-    public void undo() {
-        memoFrame.um.undo();
-    }
-    public void redo() {
-        memoFrame.um.redo();
-    }
-
     @Override
-    public void doItem(String command) {
-        if (Menu.UNDO.value().equals(command)) {
-            undo();
-        } else if (Menu.REDO.value().equals(command)) {
-            redo();
-        }
-    }
+    public abstract void doItem(String command);
 }
