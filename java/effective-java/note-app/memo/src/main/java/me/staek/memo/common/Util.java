191b0bde8d97b7c801c9a2d2b30ac0cd83233a76
seongtaekkim <chxortnl@gmail.com>
2023-10-09 14:28:02 +0900

    Fix: 인스턴스 생성 안되도록 코드 변경

--- java/effective-java/note-app/memo/src/main/java/me/staek/memo/common/Util.java
+++ java/effective-java/note-app/memo/src/main/java/me/staek/memo/common/Util.java
@@ -3,6 +3,10 @@ package me.staek.memo.common;
 import java.io.*;
 
 public class Util {
+    private Util() {
+        throw new AssertionError();
+    }
+
     public static byte[] convertObjectToBytes(Object obj) throws IOException {
         ByteArrayOutputStream boas = new ByteArrayOutputStream();
         try (ObjectOutputStream ois = new ObjectOutputStream(boas)) {
