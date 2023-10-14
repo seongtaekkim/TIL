d876dc8e59c48cd23519ffd4301ae2215ab15ff9
seongtaekkim <chxortnl@gmail.com>
2023-10-14 23:53:05 +0900

    Test: ADD 한정적 애노테이션 예제

---
+++ java/effective-java/effective-java/src/main/java/me/staek/chapter05/item33/bounded_type_token/MyAnnotation.java
@@ -0,0 +1,8 @@
+package me.staek.chapter05.item33.bounded_type_token;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyAnnotation {
+}
---
+++ java/effective-java/effective-java/src/main/java/me/staek/chapter05/item33/bounded_type_token/MyService.java
@@ -0,0 +1,5 @@
+package me.staek.chapter05.item33.bounded_type_token;
+
+@MyAnnotation
+public class MyService {
+}
