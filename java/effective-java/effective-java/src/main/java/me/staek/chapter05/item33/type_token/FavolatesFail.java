b111573c8b21d9a3173f8003569bd98ac82b1e9a
seongtaekkim <chxortnl@gmail.com>
2023-10-15 11:04:50 +0900

    Test: MOD 수정

--- java/effective-java/effective-java/src/main/java/me/staek/chapter05/item33/type_token/FavolatesFail.java
+++ java/effective-java/effective-java/src/main/java/me/staek/chapter05/item33/type_token/FavolatesFail.java
@@ -5,12 +5,12 @@ import java.util.Map;
 
 /**
  * 타입안전이종컨테이너 잘못된 설계
- * - Map<Class, Object>의 Class는 type token을 잘못 사용한 예이다.
+ * - Map<Class, Object>의 Class는 type token ㅇ
  * - key인 Class는 value인 Object에 영향을 주지 못하므로 기대한 타입과 다르게 입력되어도 실행된다는 문제가 있다.
  */
 public class FavolatesFail {
 
-    private Map<Class, Object> map = new HashMap<>();
+    private Map<Class<?>, Object> map = new HashMap<>();
     public void put(Class clazz, Object object) {
         map.put(clazz, object);
     }
