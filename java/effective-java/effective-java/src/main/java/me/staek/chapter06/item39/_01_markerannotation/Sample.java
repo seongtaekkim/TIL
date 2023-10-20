1f1b56981b963a65567304870458693bb17ec8a6
seongtaekkim <chxortnl@gmail.com>
2023-10-20 10:27:34 +0900

    Test: ADD sample code with test annotation

---
+++ java/effective-java/effective-java/src/main/java/me/staek/chapter06/item39/_01_markerannotation/Sample.java
@@ -0,0 +1,23 @@
+package me.staek.chapter06.item39._01_markerannotation;
+
+
+import me.staek.Test;
+
+public class Sample {
+    @Test
+    public static void m1() { }
+    public static void m2() { }
+    @Test
+    public static void m3() {    // Test should fail
+        throw new RuntimeException("Boom");
+    }
+    public static void m4() { }
+//    @Test
+    public void m5() { }
+    public static void m6() { }
+    @Test
+    public static void m7() {    // Test should fail
+        throw new RuntimeException("Crash");
+    }
+    public static void m8() { }
+}
