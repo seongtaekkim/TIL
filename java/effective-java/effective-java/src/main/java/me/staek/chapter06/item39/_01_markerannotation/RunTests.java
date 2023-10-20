748197b9a16a2403b7e1ba3ecc15f3ad5319d183
seongtaekkim <chxortnl@gmail.com>
2023-10-20 10:28:53 +0900

    Test: ADD 테스트 파일에 @Test 가 작성된 모든 메서드를 테스트하는 코드

---
+++ java/effective-java/effective-java/src/main/java/me/staek/chapter06/item39/_01_markerannotation/RunTests.java
@@ -0,0 +1,34 @@
+package me.staek.chapter06.item39._01_markerannotation;
+
+import me.staek.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * @Test이 작성된 메서드를 실행
+ * 1) 메서드의 예외처리 작성 (InvocationTargetException)
+ * 2) 1)이외 예외처리 작성 (non static method는 인스턴스가 없으므로 여기에 걸림)
+ */
+public class RunTests {
+    public static void main(String[] args) throws Exception {
+        int tests = 0;
+        int passed = 0;
+        Class<?> testClass = Class.forName("me.staek.chapter06.item39._01_markerannotation.Sample");
+        for (Method m : testClass.getDeclaredMethods()) {
+            if (m.isAnnotationPresent(Test.class)) {
+                tests++;
+                try {
+                    m.invoke(null);
+                    passed++;
+                } catch (InvocationTargetException wrappedExc) {
+                    Throwable exc = wrappedExc.getCause();
+                    System.out.println(m + " failed: " + exc + " " + wrappedExc.getMessage());
+                } catch (Exception exc) {
+                    System.out.println("Invalid @Test: " + m + " " + exc.getMessage());
+                }
+            }
+        }
+        System.out.printf("Passed: %d, Failed: %d%n", passed, tests - passed);
+    }
+}
