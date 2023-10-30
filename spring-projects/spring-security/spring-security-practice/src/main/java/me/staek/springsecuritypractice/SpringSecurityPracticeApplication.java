e9fb1152b3cf2e679d2fd8391f4429f80f8f76df
seongtaekkim <chxortnl@gmail.com>
2023-10-30 15:00:55 +0900

    Chore: ADD spring security 프로젝트 생성
    - spring boot 3.1.5
    - java 17
    - thymeleaf
    - spreing web

---
+++ spring-projects/spring-security/spring-security-practice/src/main/java/me/staek/springsecuritypractice/SpringSecurityPracticeApplication.java
@@ -0,0 +1,13 @@
+package me.staek.springsecuritypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityPracticeApplication.class, args);
+    }
+
+}
