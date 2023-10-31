7fc93f5f3aec08552f16b46752b6c207860d3a9c
seongtaekkim <chxortnl@gmail.com>
2023-10-31 18:55:28 +0900

    Feat: ADD JPA 인터페이스 정의

---
+++ spring-projects/spring-security/spring-security-practice/src/main/java/me/staek/springsecuritypractice/account/AccountRepository.java
@@ -0,0 +1,7 @@
+package me.staek.springsecuritypractice.account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+    Account findByUsername(String username);
+}
