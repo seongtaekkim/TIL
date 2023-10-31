2f8010287187f94d57ea5f0df09e109d67752707
seongtaekkim <chxortnl@gmail.com>
2023-10-31 19:47:18 +0900

    Refactor: PasswordEncoderFactories 으로 bcrypt 해시 알고리즘 적용

--- spring-projects/spring-security/spring-security-practice/src/main/java/me/staek/springsecuritypractice/SpringSecurityPracticeApplication.java
+++ spring-projects/spring-security/spring-security-practice/src/main/java/me/staek/springsecuritypractice/SpringSecurityPracticeApplication.java
@@ -3,6 +3,7 @@ package me.staek.springsecuritypractice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -11,11 +12,11 @@ public class SpringSecurityPracticeApplication {
 
 
     /**
-     * 패스워드 기본 encode 전략
+     * 기본 bcrypt 해시 알고리즘
      */
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     public static void main(String[] args) {
