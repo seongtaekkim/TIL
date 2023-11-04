f7ad7494c36f4760e8b8be28a664792d9c9d4c9d
seongtaekkim <chxortnl@gmail.com>
2023-11-04 21:19:47 +0900

    Test: MOD EldestEntryRemovalFunction test

--- java/effective-java/effective-java/src/main/java/me/staek/chapter07/item44/LinkedHashMap2.java
+++ java/effective-java/effective-java/src/main/java/me/staek/chapter07/item44/LinkedHashMap2.java
@@ -2,23 +2,43 @@ package me.staek.chapter07.item44;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * removeEldestEntry 재정의
  */
+
 public class LinkedHashMap2<K,V>  extends LinkedHashMap {
-    @Override
-    protected boolean removeEldestEntry(Map.Entry eldest) {
-        return size() > 3;
+
+    public void remove(EldestEntryRemovalFunction<Integer, String> ev) {
+        if (this.size() == 0)
+            throw new ArrayStoreException();
+        System.out.println("1111222");
+        Map.Entry<Integer, String> entry = (Map.Entry<Integer, String>) this.entrySet().iterator().next();
+        ev.remove(this, entry);
     }
 
     public static void main(String[] args) {
+
         LinkedHashMap2<Integer, String> li_hash =
                 new LinkedHashMap2<>();
         li_hash.put(1, "Coding");
         li_hash.put(2, "is");
         li_hash.put(3, "fun");
         li_hash.put(4, "dfef");
+
+        li_hash.remove(new EldestEntryRemovalFunction<Integer, String>() {
+            @Override
+            public boolean remove(Map<Integer, String> map, Map.Entry<Integer, String> eldest) {
+                if (map.size() > 3) {
+                    System.out.println("1111");
+                    map.remove(eldest.getKey());
+                    return true;
+                }
+                return false;
+            }
+        });
         System.out.println("" + li_hash);
+
     }
 }
