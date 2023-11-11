336c8e8a16d81d87ae81604a92b06b5e0295a26d
seongtaekkim <chxortnl@gmail.com>
2023-11-11 10:11:40 +0900

    Feature: ADD user info

---
+++ spring-projects/spring-security/spring-security-jwt/src/main/java/me/staek/springsecurityjwt/account/PrincipalDetails.java
@@ -0,0 +1,61 @@
+package me.staek.springsecurityjwt.account;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class PrincipalDetails implements UserDetails {
+
+    private Account user;
+
+    public PrincipalDetails(Account user) {
+        this.user = user;
+    }
+
+    public Account getUser() {
+        return user;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        user.getRoleList().forEach(r -> {
+            authorities.add(() -> {
+                return r;
+            });
+        });
+        return authorities;
+    }
+}
