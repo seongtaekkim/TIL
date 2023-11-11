8b7b90654f27ca5952688144304fe50749d56942
seongtaekkim <chxortnl@gmail.com>
2023-11-11 10:12:26 +0900

    Feature: ADD account service

---
+++ spring-projects/spring-security/spring-security-jwt/src/main/java/me/staek/springsecurityjwt/account/AccountService.java
@@ -0,0 +1,41 @@
+package me.staek.springsecurityjwt.account;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService implements UserDetailsService {
+
+    @Autowired AccountRepository accountRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account = accountRepository.findByUsername(username);
+        if (account == null)
+            throw new UsernameNotFoundException(username);
+
+        return new PrincipalDetails(account);
+    }
+
+    public Account createAccount(Account account) {
+        account.encodePassword(passwordEncoder);
+        this.accountRepository.save(account);
+        return account;
+    }
+
+    /**
+     * ThreadLocal은 요청(thread) 별로 제네릭 타입 정보가 격리되어 있기 때문에,
+     * 인자를 받지 않고도 해당 thread의 제네릭타입 정보를 얻을 수 있다.
+     */
+    public void printUsername() {
+        Account account = AccountContext.getAccount();
+        System.out.println(account.toString());
+    }
+}
