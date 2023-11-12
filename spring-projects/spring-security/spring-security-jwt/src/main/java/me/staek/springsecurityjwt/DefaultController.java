bb822acb59a09590bc7581b9f8ce40f44653c91f
seongtaekkim <chxortnl@gmail.com>
2023-11-12 10:10:29 +0900

    Feature: ADD default service

---
+++ spring-projects/spring-security/spring-security-jwt/src/main/java/me/staek/springsecurityjwt/DefaultController.java
@@ -0,0 +1,58 @@
+package me.staek.springsecurityjwt;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class DefaultController {
+
+    @GetMapping("/")
+    public String index(Model model, Principal principal) {
+        if (principal == null)
+            model.addAttribute("message", "hello no login user");
+        else
+            model.addAttribute("message", "login : " + principal.getName());
+        return "index";
+    }
+
+    @GetMapping("/info")
+    public String info(Model model) {
+        model.addAttribute("message", "hello world");
+        return "info";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model, Principal principal) {
+        model.addAttribute("message", "admin : " + principal.getName());
+        return "admin";
+    }
+
+    @GetMapping("/user")
+    public String user(Model model, Principal principal) {
+        System.out.println("in user controller");
+        System.out.println(principal.getName());
+        model.addAttribute("message", "Hello User, " + principal.getName());
+        return "user";
+    }
+
+    @Autowired DefaultService defaultService;
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Principal principal) {
+        model.addAttribute("message", "dashboard : " + principal.getName());
+        defaultService.printPrincipal();
+        return "dashboard";
+    }
+
+    @GetMapping("/principal")
+    public String principal(Model model, Principal principal) {
+        model.addAttribute("message", "principal : " + principal.getName());
+        defaultService.printPrincipal();
+        return "dashboard";
+    }
+}
