1d310b68a34d2b0876f702f1e073d50f83b0a3d0
seongtaekkim <chxortnl@gmail.com>
2023-11-12 10:10:59 +0900

    Feature: ADD security config setting

---
+++ spring-projects/spring-security/spring-security-jwt/src/main/java/me/staek/springsecurityjwt/config/SecondSecurityConfig.java
@@ -0,0 +1,18 @@
+package me.staek.springsecurityjwt.config;
+
+/**
+ */
+//@Configuration
+//@Order(Ordered.LOWEST_PRECEDENCE - 15)
+//public class SecondSecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(
+//                        authorize -> authorize.requestMatchers("/account/**").permitAll()
+//                );
+//        return http.build();
+//    }
+//
+//}
