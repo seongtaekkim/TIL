205d024df61a1f4aafa6a6a5270f943abc9c3bfd
seongtaekkim <chxortnl@gmail.com>
2024-01-23 20:03:45 +0900

    Chore: ADD eks springboot cicd project

---
+++ cloud/jenkins/src/eks-springboot/JenkinsfileCD.groovy
@@ -0,0 +1,65 @@
+def label = "hellonode-${UUID.randomUUID().toString()}"
+
+podTemplate(
+        label: label,
+        containers: [
+                //container image는 docker search 명령 이용
+                containerTemplate(name: "docker", image: "docker:latest", ttyEnabled: true, command: "cat"),
+                containerTemplate(name: "kubectl", image: "lachlanevenson/k8s-kubectl", command: "cat", ttyEnabled: true)
+        ],
+        //volume mount
+        volumes: [
+                hostPathVolume(hostPath: "/var/run/docker.sock", mountPath: "/var/run/docker.sock")
+        ]
+)
+        {
+            node(label) {
+                stage("Get Source") {
+                    //git url:"https://github.com/happykube/hellonode.git", branch: "main", credentialsId: "git_credential"
+                    git branch: 'jenkins', url: 'https://github.com/seongtaekkim/TIL.git'
+                }
+
+                //-- 환경변수 파일 읽어서 변수값 셋팅
+                def props = readProperties  file:"cloud/jenkins/src/demo/pipeline.properties"
+                def tag = props["version"]
+                def dockerRegistry = props["dockerRegistry"]
+                def credential_registry=props["credential_registry"]
+                def image = props["image"]
+                def deployment = props["deployment"]
+                def service = props["service"]
+                def ingress = props["ingress"]
+                def label_key = props["label_key"]
+                def label_value = props["label_value"]
+                def namespace = props["namespace"]
+
+                try {
+//                    stage("Build Microservice image") {
+//                        container("docker") {
+//                            docker.withRegistry("${dockerRegistry}", "${credential_regist ry}") {
+//                                sh "docker build -f ./cloud/jenkins/src/demo/Dockerfile -t ${image}:${tag} ."
+//                                sh "docker push ${image}:${tag}"
+//                                sh "docker tag ${image}:${tag} ${image}:latest"
+//                                sh "docker push ${image}:latest"
+//                            }
+//                        }
+//                    }
+                    stage( "Clean Up Existing Deployments" ) {
+                        container("kubectl") {
+                            sh "kubectl delete deployments -n ${namespace} -l ${label_key}=${label_value}"
+                        }
+                    }
+
+                    stage( "Deploy to Cluster" ) {
+                        container("kubectl") {
+                            sh "kubectl apply -n ${namespace} -f ${deployment}"
+                            sh "sleep 5"
+                            sh "kubectl apply -n ${namespace} -f ${service}"
+//                            sh "kubectl apply -n ${namespace} -f ${ingress}"
+                        }
+                    }
+
+                } catch(e) {
+                    currentBuild.result = "FAILED"
+                }
+            }
+        }
---
+++ cloud/jenkins/src/eks-springboot/src/main/java/me/staek/demo/HelloController.java
@@ -0,0 +1,23 @@
+package me.staek.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello World! test ";
+    }
+
+    @GetMapping("/test/test2")
+    public String test2() {
+        return "Hello World! test test2";
+    }
+}
