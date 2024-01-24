59a969a55011b1e72453d19f7c24c1bc37387da7
seongtaekkim <chxortnl@gmail.com>
2024-01-24 21:54:03 +0900

    Feat: MOD github webhook test

--- cloud/jenkins/src/eks-springboot/src/main/java/me/staek/demo/HelloController.java
+++ cloud/jenkins/src/eks-springboot/src/main/java/me/staek/demo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello World!";
+        return "Hello World! version2";
     }
 
 
