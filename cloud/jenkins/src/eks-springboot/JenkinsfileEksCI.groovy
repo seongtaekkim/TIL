4f25c403cceeb19a8a1ad03a634ef857509d8cde
seongtaekkim <chxortnl@gmail.com>
2024-01-24 18:56:28 +0900

    Chore: FIX eks ci

--- cloud/jenkins/src/eks-springboot/JenkinsfileEksCI.groovy
+++ cloud/jenkins/src/eks-springboot/JenkinsfileEksCI.groovy
@@ -31,7 +31,7 @@ pipeline {
         stage('dockerizing project by dockerfile') {
             steps {
                 sh '''
-        		 docker build -t $IMAGE_NAME:$BUILD_NUMBER .
+        		 docker build -t cloud/jenkins/src/eks-springboot/$IMAGE_NAME:$BUILD_NUMBER .
         		 docker tag $IMAGE_NAME:$BUILD_NUMBER $IMAGE_NAME:latest
 
         		 '''
